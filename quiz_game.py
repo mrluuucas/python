47c9d8e4c660e7a262e66a1b02a7285ece2deda6
mrluuucas <lucascleberlikedin@gmail.com>
2023-03-11 16:05:49 -0300

    fauston

--- quiz_game.py
+++ quiz_game.py
@@ -4,3 +4,12 @@ playing = input("Do you want play this game? YOU ARE READY?: ")
 
 if playing != "Yes":
     quit()
+
+print("Carregando jogo BIP BOP <|O_O|>")
+
+aswer = input("terra dos moinhos e ventos, E um pais da EUROPA, seu idioma e o HOLÂNDES")
+
+if aswer == "Italia":
+    print("Acertou")
+else:
+    print("Erooooooou!")
